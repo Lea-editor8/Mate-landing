8096f30352904b37803f1c36e648367bdc64be95
Leandro Barrios Pintos <leandrobarriospintos90@gmail.com>
2025-08-16 17:22:17 -0300

    Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,24 +11,12 @@ export default {
     extend: {
       colors: {
         // Paleta personalizada
-        primary: {
-          DEFAULT: '#1E40AF', // Azul base
+          primary: '#1eaaafff', // Azul base
           light: '#3B82F6',
-          dark: '#1E3A8A',
-        },
-        secondary: {
-          DEFAULT: '#F59E0B', // Amarillo base
-          light: '#FCD34D',
-          dark: '#B45309',
-        },
-        neutral: {
-          DEFAULT: '#374151', // Gris base
-          light: '#9CA3AF',
-          dark: '#111827',
-        },
+          dark: '#1E3A8A'
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+          sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
